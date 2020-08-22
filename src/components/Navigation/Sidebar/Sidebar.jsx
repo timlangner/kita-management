375773d9c004c4758ac85d2b5a32da9b7b3e23a3
Tim <tim.langner0@gmail.com>
2020-08-22 15:05:06 +0200

    :sparkles: Add sidebar

---
+++ src/components/Navigation/Sidebar/Sidebar.jsx
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import clsx from 'clsx';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import { useStyles } from "../../../utils/useStyles";
+
+export default function Sidebar({ openNav, setOpenNav }) {
+    const classes = useStyles();
+    const [isOpened, setIsOpened] = useState(openNav);
+
+    useEffect(() => {
+        if (openNav) setIsOpened(openNav)
+    }, [openNav])
+
+    const toggleDrawer = (open) => (event) => {
+        if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        } else if (!open) {
+            setOpenNav(false);
+        }
+        setIsOpened(open);
+    };
+
+    const list = (anchor) => (
+        <div
+            className={clsx(classes.list, {
+                [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+            })}
+            role="presentation"
+            onClick={toggleDrawer(anchor, false)}
+            onKeyDown={toggleDrawer(anchor, false)}
+        >
+            <List>
+                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+            <Divider />
+            <List>
+                {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+        </div>
+    );
+
+    return (
+        <div>
+            <SwipeableDrawer
+                anchor={"left"}
+                open={isOpened}
+                onClose={toggleDrawer(false)}
+                onOpen={toggleDrawer(true)}
+            >
+                {list("left")}
+            </SwipeableDrawer>
+        </div>
+    );
+}
