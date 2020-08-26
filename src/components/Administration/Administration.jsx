d6682cdf9c42695efd5acbec640de01d3790a7af
Tim <tim.langner0@gmail.com>
2020-08-26 11:43:27 +0200

    :lipstick: Remove AddChild accordion from the top

--- src/components/Administration/Administration.jsx
+++ src/components/Administration/Administration.jsx
@@ -7,9 +7,6 @@ export default function Administration() {
 
     return (
         <div style={{ margin: '15px' }}>
-            <div>
-                <AddChild />
-            </div>
             <Groups />
         </div>
     )
